23df27da17c8ebc97a28509717b19254d1d56919
Muhammad Al Fahad <85945538+muhammad-al-fahad@users.noreply.github.com>
2024-03-04 03:06:01 +0500

    Text Change in App.tsx File

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     </div>
     <div className="grid grid-cols-1 lg:grid-cols-5">
       <div className="hidden text-center lg:block">
-        
+        Men Power
       </div>
       <div className="col-span-1 text-center lg:col-span-4">
         <Game />
